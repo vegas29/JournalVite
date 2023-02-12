5cd52d2e872936a299a90d4d228a0bfefeba34d9
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-02-12 16:43:42 -0500

    Config routes

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,5 +1,9 @@
+import { AppRouter } from "./router/AppRouter";
+
 export const JournalApp = () => {
     return (
-        <div>JournalApp</div>
+        <>  
+            <AppRouter /> 
+        </>
     )
 }
