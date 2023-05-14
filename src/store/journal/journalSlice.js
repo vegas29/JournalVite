79e69c06711bdacfc9c31ee226e86843b9f6f7e3
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-05-14 11:46:06 -0500

    Borrar una nota

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -51,7 +51,9 @@ export const journalSlice = createSlice({
             state.active = null;
         },
         deleteNoteById: (state, action) => {
-
+            state.active = null;
+            state.notes = state.notes.filter(note => note.id !== action.payload);
+            state.messageSaved = `Nota eliminada correctamente`;
         }
     }
 });
